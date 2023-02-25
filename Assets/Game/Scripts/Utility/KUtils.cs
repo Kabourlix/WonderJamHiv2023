b33f8a2ea92755f4906f0509f4b6a794ff52fd49
AlexandreBuzon <77610175+AlexandreBuzon@users.noreply.github.com>
2023-02-25 16:34:48 -0500

    Add tree

--- Assets/Game/Scripts/Utility/KUtils.cs
+++ Assets/Game/Scripts/Utility/KUtils.cs
@@ -6,6 +6,7 @@ namespace Game.Scripts.Utility
     public static class KUtils
     {
         public static Random Rnd = new Random();
+        public static Vector2 xz(this Vector3 v) => v.x * Vector2.right + v.z * Vector2.up;
     }
     
     
