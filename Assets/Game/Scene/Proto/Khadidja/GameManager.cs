58767f37e0f9f9bf67ec37fbbd467c5b1cf1da16
Kabourlix <hdamaia@outlook.fr>
2023-02-25 17:31:44 -0500

    game manager working

--- Assets/Game/Scene/Proto/Khadidja/GameManager.cs
+++ Assets/Game/Scene/Proto/Khadidja/GameManager.cs
@@ -1,35 +1,76 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Game.Scripts;
+using Game.Scripts.UI;
 using UnityEngine;
+using UnityEngine.Rendering.UI;
+using UnityEngine.Serialization;
 
 public enum GameState
 {
     PlayState,
     WinState,
-    LoseState,
-    CoteState,
+    EvolveState,
+    PauseState,
+    GameOverState,
+    CaughtState,
 }
 
 public class GameManager : MonoBehaviour
 {
+    #region Singleton declaration
+
     public static GameManager Instance;
-    public GameState State;
-    public static event Action<GameState> OnGameStateChanged;
+
     private void Awake()
     {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
         Instance = this;
+        
+        //Instantiate the dictionary
+        _linksDict = new Dictionary<GameState, GameState[]>();
+        foreach (var link in links)
+        {
+            _linksDict.Add(link.DestinationState, link.ParentStates);
+        }
     }
+
+    #endregion
+
+    #region Serialized properties and their public accessors
+
+    [SerializeField] private GameStateLink[] links;
+    private Dictionary<GameState, GameState[]> _linksDict;
+    
+    [SerializeField] private Transform player;
+    public Transform Player => player;
+
+    [SerializeField] private PlayerStats stats;
+    
+    [SerializeField] private HUDManager hud;
+
+    #endregion
+    
+    
+    public GameState CurrentState { get; private set; }
+    public static event Action<GameState> OnGameStateChanged;
 
     private void Start()
     {
-        UpdateGameState(GameState.PlayState);
+        ChangeState(GameState.PlayState);
     }
 
-    public void UpdateGameState(GameState newState)
+    public void ChangeState(GameState newState)
     {
-        State = newState;
-
+        if(_linksDict[newState].Contains(CurrentState) == false) //Prevent forbidden transitions
+            throw new Exception($"Cannot transition from {CurrentState} to {newState}.");
+        
+        CurrentState = newState;
         switch (newState)
         {
             case GameState.PlayState:
@@ -38,11 +79,17 @@ public class GameManager : MonoBehaviour
             case GameState.WinState:
                 HandleWinState();
                 break;
-            case GameState.LoseState:
-                HandleLoseState();
+            case GameState.GameOverState:
+                HandleGameOverState();
                 break;
-            case GameState.CoteState:
-                HandleCoteState();
+            case GameState.CaughtState:
+                HandleCaughtState();
+                break;
+            case GameState.EvolveState:
+                HandleEvolveState();
+                break;
+            case GameState.PauseState:
+                HandlePauseState();
                 break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(newState), newState, null);
@@ -51,12 +98,29 @@ public class GameManager : MonoBehaviour
         OnGameStateChanged?.Invoke(newState);
     }
 
-    private void HandleCoteState()
+    #region State
+
+    private void HandlePauseState()
     {
-        throw new NotImplementedException();
+        Time.timeScale = 0;
+        //Hide Common UI
+        //Display the pause menu
+        //Switch controls to pause menu
     }
 
-    private void HandleLoseState()
+    private void HandleEvolveState()
+    {
+        //Nothing really
+    }
+
+    private void HandleCaughtState()
+    {
+        var hpPercentage = stats.CaughtSuspicious();
+        if(hud is not null) hud.UpdateSuspiciousBar(hpPercentage);
+        
+    }
+
+    private void HandleGameOverState()
     {
         throw new NotImplementedException();
     }
@@ -70,5 +134,14 @@ public class GameManager : MonoBehaviour
     {
         throw new NotImplementedException();
     }
+
+    #endregion
+}
+
+[Serializable]
+public struct GameStateLink
+{
+    public GameState DestinationState;
+    public GameState[] ParentStates;
 }
 
---
+++ Assets/Game/Scripts/PlayerStats.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Game.Scripts
+{
+    public class PlayerStats : MonoBehaviour
+    {
+        [SerializeField] private int maxHealth = 5;
+        private int _currentHealth;
+        public int CurrentHealth => _currentHealth;
+        private GameManager _gameManager;
+        
+        private void Start()
+        {
+            _currentHealth = maxHealth;
+            _gameManager = GameManager.Instance;
+        }
+
+        public float CaughtSuspicious()
+        {
+            _currentHealth--;
+            if (_currentHealth <= 0)
+            {
+                _gameManager.ChangeState(GameState.GameOverState);
+                return 0;
+            }
+
+            return Mathf.InverseLerp(0, (float) maxHealth, (float)_currentHealth);
+        }
+    }
+}
