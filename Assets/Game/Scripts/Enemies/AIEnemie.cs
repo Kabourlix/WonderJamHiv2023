75e0159b24a0c60f4e0a4b71150378bf73876175
AlexandreBuzon <77610175+AlexandreBuzon@users.noreply.github.com>
2023-02-25 20:12:10 -0500

    add freez

--- Assets/Game/Scripts/Enemies/AIEnemie.cs
+++ Assets/Game/Scripts/Enemies/AIEnemie.cs
@@ -6,9 +6,22 @@ using UnityEngine.AI;
 
 public class AIEnemie : MonoBehaviour
 {
-
+    [SerializeField] private MBTExecutor mBT;
+    [SerializeField] private NavMeshAgent agent;
     private void Start()
     {
-        GetComponent<PlayerController>().IsSuspect = true;
+        mBT = transform.GetChild(0).GetComponent<MBTExecutor>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+    public void Freeze(bool state)
+    {
+        mBT.freez = state;
+        agent.isStopped = state;
+    }
+    public IEnumerator FreezCoroutine(float freezTime)
+    {
+        Freeze(true);
+        yield return new WaitForSeconds(freezTime);
+        Freeze(false);
     }
 }
