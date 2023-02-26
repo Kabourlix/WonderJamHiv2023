55219a43604b352ec6cab3b1a3c00244a150da64
lhautevi <95317729+lhautevi@users.noreply.github.com>
2023-02-26 12:36:04 -0500

    scripts

--- Assets/Game/Scripts/Systems/Interaction/MakeMimic.cs
+++ Assets/Game/Scripts/Systems/Interaction/MakeMimic.cs
@@ -1,26 +1,34 @@
 ﻿using System;
+using Game.Scripts.Enemies;
 using Game.Scripts.Quests;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace Game.Scripts.Interaction
 {
     [RequireComponent(typeof(StatTriggerComponent))]
-    public class PoisonWell : MonoBehaviour, IInteractable
+    public class MakeMimic : MonoBehaviour, IInteractable
     {
         private StatTriggerComponent _statTriggerComponent;
-        
 
         private void Awake()
         {
             _statTriggerComponent = GetComponent<StatTriggerComponent>();
-            
+           
         }
+
 
         public void VFX()
         {
+            
+           
+
             Debug.Log("Interacting with" + gameObject.name);
             transform.GetChild(0).gameObject.SetActive(true);
             transform.GetChild(1).gameObject.SetActive(true);
+            transform.GetChild(2).gameObject.SetActive(false);
+
+
         }
 
         public void Interact()
@@ -28,7 +36,7 @@ namespace Game.Scripts.Interaction
             if (!_statTriggerComponent.Trigger()) return;
             VFX();
             OnInteractionSuccess();
-            
+
         }
 
         public void OnInteractionSuccess()
