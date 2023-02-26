392a3b41f501069a9f1513b2d3f72d31e948c7bd
lhautevi <95317729+lhautevi@users.noreply.github.com>
2023-02-26 00:37:17 -0500

    assets again

---
+++ Assets/Game/Scripts/Interaction/KillAnimal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Game.Scripts.Quests;
+using UnityEngine;
+
+namespace Game.Scripts.Interaction
+{
+    [RequireComponent(typeof(StatTriggerComponent))]
+    public class KillAnimal : MonoBehaviour, IInteractable
+    {
+        private StatTriggerComponent _statTriggerComponent;
+        [SerializeField] private GameObject sound;
+
+        private void Awake()
+        {
+            _statTriggerComponent = GetComponent<StatTriggerComponent>();
+        }
+
+        public void Interact()
+        {
+            Debug.Log("Interacting with" + gameObject.name);
+            _statTriggerComponent.Trigger();
+            transform.GetChild(0).gameObject.SetActive(false);
+            transform.GetChild(1).gameObject.SetActive(true);
+            transform.GetComponent<CapsuleCollider>().isTrigger = false;
+            Instantiate(sound, transform);
+        }
+    }
+}
