170854b20838202afae0028095d100bc3652735a
lhautevi <95317729+lhautevi@users.noreply.github.com>
2023-02-26 15:33:37 -0500

    push

---
+++ Assets/Game/Scripts/Systems/Interaction/RemoveTree.cs
@@ -0,0 +1,25 @@
+﻿using Game.Scripts.Quests;
+using UnityEngine;
+
+namespace Game.Scripts.Systems.Interaction
+{
+    [RequireComponent(typeof(StatTriggerComponent))]
+    public class RemoveTree : Interactable
+    {
+        public void VFX()
+        {
+            Debug.Log("Interacting with" + gameObject.name);
+            transform.GetChild(0).gameObject.SetActive(true);
+            transform.GetChild(1).gameObject.SetActive(false);
+            
+        }
+
+        public override void Interact()
+        {
+            if (!_statTriggerComponent.Trigger()) return;
+            VFX();
+            OnInteractionSuccess();
+
+        }
+    }
+}
